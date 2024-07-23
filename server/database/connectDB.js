3b1a14d4ff215d8e47570d2bd05a89664e7e9a14
Vedveyaas <gshivamg8851@gmail.com>
2024-07-23 18:42:08 +0530

    commit

--- server/database/connectDB.js
+++ server/database/connectDB.js
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
+const options = {
+    serverSelectionTimeoutMS: 30000, // Increase timeout to 30 seconds
+    socketTimeoutMS: 45000 // Close sockets after 45 seconds of inactivity
+  };
+
 const connectDB = async (DATABASE_URL) => {
     try {
-        await mongoose.connect(DATABASE_URL);
+        await mongoose.connect(DATABASE_URL,options);
         console.log("MongoDB connected successfully !!");
     } catch (error) {
         console.log(error);
